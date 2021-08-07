f6b0a83096ec38c8e21f7dd574e6ab2afd1afb0a
Ruchika Mahajan <ruchikamahajan@Ruchikas-MacBook-Air.local>
2021-08-07 19:05:50 +0530

    changed to view data

--- nearest_nbr_jijcalculation/resultViewer.py
+++ nearest_nbr_jijcalculation/resultViewer.py
@@ -8,29 +8,30 @@ def exchange_interaction(exchange_coupling_pk):
 
         print("Energy is :: ", E, "Volume is :: ", V, "Units is :: ", units, "label is :: ", label)
         labels = label.split("_")
-        if labels[1] in finalResultDict:
-            if labels[0] in finalResultDict[labels[1]]:
-                finalResultDict[labels[1]][labels[0]].add(labels[2], E)
-            else:
-                temp = {
-                    labels[2]: E
-                }
-                finalResultDict[labels[1]].add(labels[0], temp)
-        else:
-            temp = {
-                labels[0]: {
-                    labels[2]: E
-                }
-            }
-            finalResultDict.add(labels[1], temp)
-
-    for pair, value in finalResultDict.items():
-        for superCellNum, spinValues in value.items():
-            print(" JIJ value with pair :: " + pair + " and with super Cell number :: " + str(
-                superCellNum) + "result :: ",
-                  (spinValues['a'] - spinValues['b'] -
-                   spinValues['c'] + spinValues['d']) / (4 * 1.5 * 1.5))
+    #     if labels[1] in finalResultDict:
+    #         if labels[0] in finalResultDict[labels[1]]:
+    #             finalResultDict[labels[1]][labels[0]].add(labels[2], E)
+    #         else:
+    #             temp = {
+    #                 labels[2]: E
+    #             }
+    #             finalResultDict[labels[1]].add(labels[0], temp)
+    #     else:
+    #         temp = {
+    #             labels[0]: {
+    #                 labels[2]: E
+    #             }
+    #         }
+    #         finalResultDict.add(labels[1], temp)
+    #
+    # for pair, value in finalResultDict.items():
+    #     for superCellNum, spinValues in value.items():
+    #         print(" JIJ value with pair :: " + pair + " and with super Cell number :: " + str(
+    #             superCellNum) + "result :: ",
+    #               (spinValues['a'] - spinValues['b'] -
+    #                spinValues['c'] + spinValues['d']) / (4 * 1.5 * 1.5))
 
 
 if __name__ == '__main__':
-    exchange_interaction(10717)
+    exchange_interaction(3811)
+
